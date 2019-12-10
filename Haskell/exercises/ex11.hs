40cc7c7be74fd04e379a2b9ebc8819cb404a554a
Pieve <pievetp@gmail.com>
2019-12-10 10:24:45 -0300

    distância dado o tempo

--- Haskell/exercises/ex11.hs
+++ Haskell/exercises/ex11.hs
@@ -1,10 +1,8 @@
 -- Autor: Dilson Gabriel Pieve
-
-
 -- v, vo e t
 
 deltaDist :: Float -> Float -> Float -> Float
 deltaDist v vo t = vo * t + (a * t * t) / 2
-					where
-						a = (v - vo) / 2 
+			where
+				a = (v - vo) / 2 
 
