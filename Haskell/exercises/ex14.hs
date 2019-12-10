4a2e2e6b0e87a405ee915376d67b288309bdb6b6
Pieve <pievetp@gmail.com>
2019-12-10 10:27:00 -0300

    n-esimo da lista

--- Haskell/exercises/ex14.hs
+++ Haskell/exercises/ex14.hs
@@ -3,10 +3,10 @@
 
 nEsimo :: Int -> [Int] -> Int
 nEsimo x y = nEsimoAux x 1 y
-				where
-					nEsimoAux _ _ [] = -1 								-- wrong input
-					nEsimoAux x p (h:t) = if x == p then h 
-											else nEsimoAux x (p + 1) t
+		where
+			nEsimoAux _ _ [] = -1 								-- if wrong input
+			nEsimoAux x p (h:t) = if x == p then h 
+						else nEsimoAux x (p + 1) t
 
 -- fim ex14
 
