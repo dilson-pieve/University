c59e5afe47948c6b5bed4c9d09879c40753ac60c
Pieve <pievetp@gmail.com>
2019-12-10 10:26:09 -0300

    fibo + where Aux function

--- Haskell/exercises/ex13.hs
+++ Haskell/exercises/ex13.hs
@@ -3,10 +3,10 @@
 
 fibo :: Int -> Int
 fibo n = fiboAux n
-			where	
-				fiboAux 0 = 1
-				fiboAux 1 = 1
-				fiboAux n = fiboAux (n - 1) + fiboAux (n - 2)
+	where	
+		fiboAux 0 = 1
+		fiboAux 1 = 1
+		fiboAux n = fiboAux (n - 1) + fiboAux (n - 2)
 
 -- fim ex13
 
